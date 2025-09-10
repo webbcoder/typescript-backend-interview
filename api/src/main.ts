0c1bbca3cbfb3e67aaa5ccff2280c79c712f2a54
webbcoder <ksk.coder@gmail.com>
2025-09-10 01:11:56 -0600

    [DEV] Add default version to controller, add endpoints to student controller.

--- api/src/main.ts
+++ api/src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
   app.enableVersioning({
     type: VersioningType.URI,
+    defaultVersion: '1',
   })
   app.setGlobalPrefix('api')
 
--- api/src/student/student.controller.ts
+++ api/src/student/student.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Param, Post, Body, Delete, Get } from '@nestjs/common'
 
-@Controller('student')
-export class StudentController {}
+import { StudentService } from './student.service'
+import { StudentEnrollDto } from './dto/student-enroll.dto'
+
+@Controller('student/:id')
+export class StudentController {
+  constructor(private studentService: StudentService) {}
+
+  @Get(':id/schedule')
+  schedule(@Param('id') id: string) {
+    return this.studentService.schedule(id)
+  }
+  @Post('enroll')
+  enroll(@Param('id') id: string, @Body() dto: StudentEnrollDto) {
+    return this.studentService.enroll(id, dto)
+  }
+  @Delete('sections/:sectionId')
+  unenroll(@Param('id') id: string, @Param('sectionId') sectionId: string) {
+    return this.studentService.unenroll(id, sectionId)
+  }
+}
